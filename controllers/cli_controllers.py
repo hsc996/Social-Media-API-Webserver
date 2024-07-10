2d5f8624f746cadf851afd573b54ba36b74bf5d1
hsc996 <han.scaife@gmail.com>
2024-07-10 12:14:36 +1000

    user models/schema added and cli controllers tested

---
+++ controllers/cli_controllers.py
@@ -0,0 +1,40 @@
+from flask import Blueprint
+
+from init import db, bcrypt
+
+from models.user import User
+
+db_commands = Blueprint("db", __name__)
+
+@db_commands.cli.command("create")
+def create_tables():
+    db.create_all()
+    print("Tables created")
+
+
+@db_commands.cli.command("drop")
+def drop_tables():
+    db.drop_all()
+    print("Tables dropped.")
+
+
+@db_commands.cli.command("seed")
+def seed_tables():
+    users = [
+        User(
+            username="admin",
+            email="admin@email.com",
+            password="123456",
+            is_admin=True
+        ),
+        User(
+            username="username1",
+            email="user1@email.com",
+            password="123456"
+        )
+    ]
+
+    db.session.add_all(users)
+    db.session.commit()
+    print("Tables seeded.")
+
