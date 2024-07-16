bb46c38224f53fb14dd9eb3dbc92a12439cff1dd
hsc996 <han.scaife@gmail.com>
2024-07-16 13:55:14 +1000

    validation handling added to post controllers

--- controllers/post_controller.py
+++ controllers/post_controller.py
@@ -3,6 +3,7 @@ from datetime import date
 from flask import Blueprint, request
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from sqlalchemy.orm.exc import NoResultFound
+from marshmallow import ValidationError
 
 from init import db, ma
 from models.post import Post, post_schema, posts_schema
@@ -37,20 +38,28 @@ def get_single_post(post_id):
 @posts_bp.route("/", methods=["POST"])
 @jwt_required()
 def create_post():
-    body_data = request.get_json()
+    try:
+        body_data = request.get_json()
 
-    if not body_data or not body_data.get("body"):
-        return {"error": "Invalid request body"}, 400
+        if not body_data or not body_data.get("body"):
+            return {"error": "Invalid request body"}, 400
+        
+        post = Post(
+            body=body_data.get("body"),
+            timestamp=date.today(),
+            user_id=get_jwt_identity()
+        )
+        db.session.add(post)
+        db.session.commit()
+
+        return post_schema.dump(post)
     
-    post = Post(
-        body=body_data.get("body"),
-        timestamp=date.today(),
-        user_id=get_jwt_identity()
-    )
-    db.session.add(post)
-    db.session.commit()
-
-    return post_schema.dump(post)
+    except ValidationError as e:
+        return {"error": str(e)}, 400
+    
+    except Exception as e:
+        db.session.rollback()
+        return {"error": "Internal Server Error"}, 500
 
 
 
@@ -58,10 +67,10 @@ def create_post():
 @posts_bp.route("/<int:post_id>", methods=["DELETE"])
 @jwt_required()
 def delete_post(post_id):
-    stmt = db.select(Post).filter_by(id=post_id)
-    post = db.session.scalar(stmt)
-
     try:
+        stmt = db.select(Post).filter_by(id=post_id)
+        post = db.session.scalar(stmt)
+
         is_admin = authorise_as_admin()
         if not is_admin or str(post.user_id) != get_jwt_identity():
             return {"error": f"Unauthorized to delete: you are not the owner of this post."}, 403
@@ -73,18 +82,22 @@ def delete_post(post_id):
     
     except NoResultFound:
         return {"error":f"Post with ID {post_id} not found"}, 404
-
+    
+    except Exception as e:
+        db.session.rollback()
+        return {"error": "Internal Server Error"}, 500
+    
 
 
 # /posts/<id> - PUT, PATCH - edit a post
 @posts_bp.route("/<int:post_id>", methods=["PUT", "PATCH"])
 @jwt_required()
 def update_post(post_id):
-    body_data = request.get_json()
-    stmt = db.select(Post).filter_by(id=post_id)
-    post = db.session.scalar(stmt)
-
     try:
+        body_data = request.get_json()
+        stmt = db.select(Post).filter_by(id=post_id)
+        post = db.session.scalar(stmt)
+
         is_admin = authorise_as_admin()
         if not is_admin or str(post.user_id) != get_jwt_identity():
             return {"error": f"Unauthorized to delete: you are not the owner of this post."}, 403
@@ -95,6 +108,13 @@ def update_post(post_id):
         db.session.commit()
         
         return post_schema.dump(post)
+    
+    except ValidationError as e:
+        return {"error": str(e)}, 400
+
     except NoResultFound:
         return {"error":f"Post with ID {post_id} not found."}, 404
+    
+    except Exception as e:
+        return {"error": "Internal Server Error"}, 500
     
